84745b12e93f013164ff982957030f3a2953cd2a
Tanjil <tanjilkhan.sh@gmail.com>
2020-05-20 09:52:53 +0100

    Add files via upload

---
+++ format/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+
+    int slices;
+    cout << "Type an number: ";
+    cin >> slices;
+    printf("The number of the pizza slices is %i\n", slices);
+    return 0;
+}
